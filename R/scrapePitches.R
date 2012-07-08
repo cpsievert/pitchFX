afcb93155bfff990b45ee031777efb4c51ad52d8
Carson Sievert <cpsievert1@gmail.com>
2012-07-08 18:28:09 -0500

    add all_games functionality

--- R/scrapePitches.R
+++ R/scrapePitches.R
@@ -6,6 +6,7 @@
 #' 
 #' @param start date "yyyy-mm-dd" to commence scraping of pitch F/X data
 #' @param end date "yyyy-mm-dd" to terminate scraping pitch F/X data
+#' @param game.data data set with game URLs
 #' @return Returns a list containing two different data frames. The larger data frame contains data on every pitch thrown (pitch F/X). The smaller one contains data on every atbat.
 #' @export
 #' @examples
@@ -20,14 +21,14 @@
 #' unique(pitchFX$pitch_type)
 #' pitchFX <- pitchFX[pitchFX$pitch_type == c("SL","CU"), ]
 
-scrapePitchFX <- function(start = "2012-01-01", end = Sys.Date()) { #make fields flexible?
-  first.date <- paste("('", start, "')", sep = "")
+scrapePitchFX <- function(start = "2012-01-01", end = Sys.Date(), game.data = all_games) { #make fields flexible?
+  start <- as.POSIXct(start)
+  end <- as.POSIXct(end)
   if (year(start) < 2005) stop("Not only is pitchFX data not avaliable before 2008, data on each game isn't complete until 2005")
   if (year(start) < 2008) stop("Warning: pitchFX data wasn't recorded consistently until 2008. Please consider a later start date.")
-  last.date <- paste("('", end, "')", sep = "")
   if (end > Sys.Date()) stop("Sorry, I can't scrape data on the future!")
-  urls <- dbGetQuery(MLB, paste("SELECT url FROM all_games WHERE all_games.date >= ", first.date, "AND all_games.date <=", last.date, sep = ""))
-  u <- urls[,"url"]
+  all.games <- data(game.data)
+  urls <- all.games[(all.games$date > start & all.games$date < end), "url"]
   p <- c("ax", "ay", "az", "break_angle", "break_length", "break_y", "cc", "des", 
                 "end_speed", "id", "mt", "nasty", "on_1b", "on_2b", "on_3b", "pfx_x", "pfx_z", "pitch_type", 
                 "px", "pz", "spin_dir", "spin_rate", "start_speed", "sv_id", "sz_bot", "sz_top", 
